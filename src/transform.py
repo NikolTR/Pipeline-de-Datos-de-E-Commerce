a1f2e1b7cdcaeeddd2b48344590be5e8653882d2
Nikkol <nikol.tamayo@est.iudigital.edu.com>
2025-03-18 06:35:34 -0500

    Se agrega el código de transformaciones

--- src/transform.py
+++ src/transform.py
@@ -171,14 +171,16 @@ def query_freight_value_weight_relationship(database: Engine) -> QueryResult:
 
     # TODO: Fusionar las tablas items, orders y products usando 'order_id'/'product_id'.
     # Sugerimos usar la función pandas.merge().
-    # Asigna el resultado a la variable `data`.
-    data = ...
+
+    data = pd.merge(olist_order_items, olist_orders, on='order_id', how='inner')
+    data = pd.merge(data, olist_products, on='product_id', how='inner')
 
     # TODO: Obtener solo los pedidos entregados.
     # Usando los resultados anteriores de la fusión (almacenados en la variable `data`),
     # aplica una máscara booleana para conservar solo los pedidos con estado 'delivered'.
     # Asigna el resultado a la variable `delivered`.
-    delivered = ...
+    
+    delivered = data[data['order_status'] == 'delivered']
 
     # TODO: Obtener la suma de freight_value y product_weight_g por cada order_id.
     # Un mismo pedido (identificado por 'order_id') puede contener varios productos,
@@ -188,7 +190,10 @@ def query_freight_value_weight_relationship(database: Engine) -> QueryResult:
     # que consultes pandas.DataFrame.groupby() y pandas.DataFrame.agg() para la
     # transformación de los datos.
     # Guarda el resultado en la variable `aggregations`.
-    aggregations = ...
+    aggregations = delivered.groupby('order_id').agg({
+    'freight_value': 'sum',
+    'product_weight_g': 'sum'
+    }).reset_index()
 
     # Mantén el código a continuación tal como está, esto devolverá el resultado de
     # la variable `aggregations` con el nombre y formato correspondiente.
@@ -223,19 +228,25 @@ def query_orders_per_day_and_holidays_2017(database: Engine) -> QueryResult:
     # Reemplaza el contenido de la columna `order_purchase_timestamp` en el DataFrame `orders`
     # con los mismos datos pero convertidos a tipo datetime.
     # Te sugerimos leer sobre cómo usar pd.to_datetime() para esto.
-    orders["order_purchase_timestamp"] = ...
+    
+    olist_orders["order_purchase_timestamp"] = pd.to_datetime(olist_orders["order_purchase_timestamp"])
 
     # TODO: Filtrar solo las fechas de compra de pedidos del año 2017.
     # Usando el DataFrame `orders`, aplica una máscara booleana para obtener todas las
     # columnas, pero solo las filas correspondientes al año 2017.
     # Asigna el resultado a una nueva variable llamada `filtered_dates`.
-    filtered_dates = ...
+
+    filtered_dates = olist_orders[olist_orders["order_purchase_timestamp"].dt.year == 2017]
 
     # TODO: Contar la cantidad de pedidos por día.
     # Usando el DataFrame `filtered_dates`, cuenta cuántos pedidos se hicieron
     # cada día.
     # Asigna el resultado a la variable `order_purchase_ammount_per_date`.
-    order_purchase_ammount_per_date = ...
+    order_purchase_ammount_per_date = (
+    filtered_dates.groupby(filtered_dates["order_purchase_timestamp"].dt.date)
+    .size()
+    .reset_index(name="order_count")
+    )
 
     # TODO: Crear un DataFrame con el resultado. Asígnalo a la variable `result_df`.
     # Ahora crearemos el DataFrame final para la salida.
